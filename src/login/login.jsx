c67f6648119695505bbb2717779feb7d87c1db0c
Abbie Omer <aomer942@byu.edu>
2025-02-11 11:21:24 -0700

    implemented login components, need to fix spacing between buttons

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className='body'>
-      <div>login displayed here</div>
-    </main>
+    <main>
+    <form method="get" action="studyroom.html">
+        <div>
+        <label for="username">Username: </label>
+        <input type="text" className="input" name="varUsername" placeholder="Username"/>
+        </div>
+        <br />
+        <div>
+        <label for="password">Password: </label>
+        <input type="password" className="input" name="varPassword" placeholder="Password" />
+            
+</div>
+<br />
+            <button type="submit" className="btn btn-primary">Login</button>
+            <button type="submit" className="btn btn-primary">Register</button>
+          </form>
+      
+</main>
   );
 }
