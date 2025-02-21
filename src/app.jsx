e02a1b627f6a7cdb0428938c5c8360416080e4b5
Abbie Omer <aomer942@byu.edu>
2025-02-21 10:16:25 -0700

    Made it so that only authorized users can navigate to studyroom and projects

--- src/app.jsx
+++ src/app.jsx
@@ -4,10 +4,14 @@ import './app.css';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Login } from './login/login';
 import { Studyroom } from './studyroom/studyroom';
+import { AuthState } from './login/authState';
 import { Projects } from './projects/projects';
 import { About } from './about/about';
 
 export default function App() {
+  const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+  const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+  const [authState, setAuthState] = React.useState(currentAuthState);
   return (
     <BrowserRouter>
   <div className='body'>
@@ -16,8 +20,8 @@ export default function App() {
   <nav>
   <menu className="navbar-nav">
       <li className="nav-item"><NavLink className="nav-link active"to="login">Login</NavLink></li>
-      <li className="nav-item"><NavLink className="nav-link active"to="studyroom">StudyRoom</NavLink></li>
-      <li className="nav-item"><NavLink className="nav-link active"to="projects">Project Completion Tracker</NavLink></li>
+      {authState === AuthState.Authenticated && (<li className="nav-item"><NavLink className="nav-link active"to="studyroom">StudyRoom</NavLink></li>)}
+      {authState === AuthState.Authenticated && (<li className="nav-item"><NavLink className="nav-link active"to="projects">Project Completion Tracker</NavLink></li>)}
       <li className="nav-item"><NavLink className="nav-link active"to="about">About</NavLink></li>
     </menu>
 </nav>
@@ -26,8 +30,22 @@ export default function App() {
 </header>
 
 <Routes>
-  <Route path='/' element={<Login />} exact />
-  <Route path='/login' element={<Login />} exact />
+  <Route path='/' element={<Login
+                userName={userName}
+                authState={authState}
+                onAuthChange={(userName, authState) => {
+                  setAuthState(authState);
+                  setUserName(userName);
+                }}
+              />} exact />
+  <Route path='/login' element={<Login
+                userName={userName}
+                authState={authState}
+                onAuthChange={(userName, authState) => {
+                  setAuthState(authState);
+                  setUserName(userName);
+                }}
+              />} exact />
   <Route path='/studyroom' element={<Studyroom />} />
   <Route path='/projects' element={<Projects />} />
   <Route path='/about' element={<About />} />
